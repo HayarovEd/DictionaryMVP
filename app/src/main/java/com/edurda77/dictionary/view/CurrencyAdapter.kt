9bab29020266685629ccddb93b9837653d38a9e0
hayarovy <edurd@rambler.ru>
2022-03-22 12:16:24 +0500

    maked holder, adapter and layout

---
+++ app/src/main/java/com/edurda77/dictionary/view/CurrencyAdapter.kt
@@ -0,0 +1,25 @@
+import TranslateHolder
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.edurda77.dictionary.model.data.datasource.WordTranslate
+
+
+class CurrencyAdapter(
+    private val list: List<WordTranslate>,
+) :
+    RecyclerView.Adapter<TranslateHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TranslateHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        return TranslateHolder(inflater, parent)
+    }
+
+    override fun onBindViewHolder(holder: TranslateHolder, position: Int) {
+        val wordTranslate: WordTranslate = list[position]
+        holder.bind(wordTranslate)
+    }
+
+    override fun getItemCount(): Int = list.size
+}
+
---
+++ app/src/main/java/com/edurda77/dictionary/view/CurrencyHolder.kt
@@ -0,0 +1,26 @@
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.edurda77.dictionary.R
+import com.edurda77.dictionary.model.data.datasource.WordTranslate
+
+class TranslateHolder(inflater: LayoutInflater, parent: ViewGroup) :
+    RecyclerView.ViewHolder(inflater.inflate(R.layout.item_translate, parent, false)) {
+    private var textItem: TextView? = null
+    private var translateItem: TextView? = null
+
+    init {
+        textItem = itemView.findViewById(R.id.text_item)
+        translateItem = itemView.findViewById(R.id.translate_item)
+    }
+
+    @SuppressLint("SetTextI18n")
+    fun bind(wordTranslate: WordTranslate) {
+        textItem?.text = wordTranslate.text
+        wordTranslate.meanings.forEach {
+            translateItem?.text = it.translation.toString() + ", "
+        }
+    }
+}
